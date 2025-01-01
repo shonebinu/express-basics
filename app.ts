441b119539d9820c71287c066d49e44fb3b44f40
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-01-01 20:38:05 +0530

    setup views directory

--- app.ts
+++ app.ts
@@ -2,8 +2,16 @@ import express, { ErrorRequestHandler } from "express";
 import authorRouter from "./routes/authorRouter";
 import bookRouter from "./routes/bookRouter";
 import indexRouter from "./routes/indexRouter";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
